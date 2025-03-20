ffa753eb175a2265625e9f0aaa80941d3534aba0
ninan.ni <ninan.ni@alibaba-inc.com>
2025-03-20 18:04:25 +0800

    image-process

--- src/code/index.py
+++ src/code/index.py
@@ -1,47 +1,84 @@
 # -*- coding: utf-8 -*-
 
-import oss2
 import json
 from wand.image import Image
 from wand.drawing import Drawing
 from wand.color import Color
 import base64
+import requests  # 新增：用于下载图片
+import uuid
+import oss2
+import os
+import logging
+
+# OSS 配置
+OSS_SECURITY_TOKEN = os.getenv("ALIBABA_CLOUD_SECURITY_TOKEN")
+OSS_ACCESS_KEY_ID = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+OSS_ACCESS_KEY_SECRET = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+OSS_ENDPOINT = os.getenv("OSS_ENDPOINT")
+OSS_BUCKET = os.getenv("OSS_BUCKET")
+
+def upload_to_oss(img_blob, img_type):
+
+    # 创建 Bucket 实例
+    auth = oss2.StsAuth(OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET, OSS_SECURITY_TOKEN)
+    bucket = oss2.Bucket(auth, OSS_ENDPOINT, OSS_BUCKET)
+
+    # 生成唯一的文件名
+    file_name = f"{uuid.uuid4()}.{img_type}"
+    print("filename: " + file_name)
+    
+    # 上传文件
+    try:
+        result = bucket.put_object(file_name, img_blob)
+        logging.info(f"File uploaded successfully, status code: {result.status}")
+    except oss2.exceptions.OssError as e:
+        logging.error(f"Failed to upload file: {e}")
+
+    # 生成公开访问的 URL
+    return f"https://{OSS_BUCKET}.{OSS_ENDPOINT}/{file_name}"
 
 
-def oss_get_image_data(context, path):
-    parts = path.split("/")
-    bucket = parts[0]
-    object = "/".join(parts[1:])
-    creds = context.credentials
-    auth = oss2.StsAuth(creds.accessKeyId, creds.accessKeySecret, creds.securityToken)
-    oss_client = oss2.Bucket(
-        auth, "oss-" + context.region + "-internal.aliyuncs.com", bucket
-    )
-    return oss_client.get_object(object)
+def download_image(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        raise Exception(f"Failed to download image from {url}")
+    
+def make_error_response(ex):
+    return {
+        "statusCode": 400,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"error": str(ex)})
+    }
 
-
-def oss_save_image_data(context, img_blob, path):
-    parts = path.split("/")
-    bucket = parts[0]
-    object = "/".join(parts[1:])
-    creds = context.credentials
-    auth = oss2.StsAuth(creds.accessKeyId, creds.accessKeySecret, creds.securityToken)
-    oss_client = oss2.Bucket(
-        auth, "oss-" + context.region + "-internal.aliyuncs.com", bucket
-    )
-    print(bucket, object)
-    oss_client.put_object(object, img_blob)
-
+def make_response(context, query, img_blob, img_type):
+    
+    print("image blob length:", len(img_blob))
+    
+    # 上传到 OSS 并获取 URL
+    oss_url = upload_to_oss(img_blob, img_type)
+    
+    response = {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({
+            "imageUrl": oss_url,
+            "imageType": img_type
+        })
+    }
+    return response
 
 def pinjie(query, context):
     try:
-        image1 = query.get("left")
-        image2 = query.get("right")
-        assert image1 and image2
+        image1_url = query.get("left")
+        image2_url = query.get("right")
+        assert image1_url and image2_url
         fmt = query.get("fmt", "jpg")
-        print("pinjie images: ", image1, image2)
-        with Image(file=oss_get_image_data(context, image1)) as f1:
-            with Image(file=oss_get_image_data(context, image2)) as f2:
+        print("pinjie images: ", image1_url, image2_url)
+        with Image(blob=download_image(image1_url)) as f1:
+            with Image(blob=download_image(image2_url)) as f2:
                 with Image(
                     width=f1.width + f2.width + 10, height=max(f1.height, f2.height)
                 ) as img:
@@ -52,14 +89,13 @@ def pinjie(query, context):
     except Exception as ex:
         return make_error_response(ex)
 
-
 def watermark(query, context):
     try:
-        image = query.get("img")
+        image_url = query.get("img")
         text = query.get("text")
-        assert image and text
+        assert image_url and text
         fmt = query.get("fmt", "jpg")
-        print("watermark image: {}, text: {}".format(image, text))
+        print("watermark image: {}, text: {}".format(image_url, text))
         with Drawing() as draw:
             draw.font = "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc"
             draw.fill_color = Color("red")
@@ -71,7 +107,7 @@ def watermark(query, context):
                 pic.format = "png"
                 pic.save(filename="/tmp/water.png")
 
-        with Image(file=oss_get_image_data(context, image)) as img:
+        with Image(blob=download_image(image_url)) as img:
             with Image(filename="/tmp/water.png") as water:
                 with img.clone() as base:
                     base.format = fmt
@@ -80,98 +116,53 @@ def watermark(query, context):
     except Exception as ex:
         return make_error_response(ex)
 
-
 def format(query, context):
     try:
-        image = query.get("img")
+        image_url = query.get("img")
         fmt = query.get("fmt")
-        assert image and fmt
-        print("format image: {}, fmt: {}".format(image, fmt))
-        with Image(file=oss_get_image_data(context, image)) as img:
+        assert image_url and fmt
+        print("format image: {}, fmt: {}".format(image_url, fmt))
+        with Image(blob=download_image(image_url)) as img:
             img.format = fmt
             return make_response(context, query, img.make_blob(), fmt)
     except Exception as ex:
         return make_error_response(ex)
 
-
 def gray(query, context):
     try:
-        img_path = query.get("img")
-        parts = img_path.split("/")
-        object = "/".join(parts[1:])
-        img_type = object.split(".")[-1]
-        if (
-            img_type != "jpg"
-            and img_type != "jpeg"
-            and img_type != "webp"
-            and img_type != "png"
-        ):
+        img_url = query.get("img")
+        img_type = img_url.split(".")[-1].lower()
+        if img_type not in ["jpg", "jpeg", "webp", "png"]:
             img_type = "jpeg"
 
-        with Image(file=oss_get_image_data(context, img_path)) as img:
+        with Image(blob=download_image(img_url)) as img:
             img.format = img_type
             img.transform_colorspace("gray")
             return make_response(context, query, img.make_blob(), img_type)
     except Exception as ex:
         return make_error_response(ex)
-
-
-def make_error_response(ex):
-    return {
-        "body": "ERROR: {}".format(str(ex)),
-        "headers": {"content-type": "text/plain"},
-        "statusCode": 500,
-    }
-
-
-def make_response(context, query, img_blob, img_type):
-    target = query.get("target")
-    if target is not None and len(target) > 0:
-        oss_save_image_data(context, img_blob, target)
-    base64Data = base64.b64encode(img_blob).decode()
-    dataUrl = "data:image/{};base64,{}".format(img_type, base64Data)
-    html = """
-<div style="text-align: center;">
-<img src="{}" alt="Base64 Image" style="display: inline-block;">
-</div>
-""".format(
-        dataUrl
-    )
-    return {
-        "body": html,
-        "headers": {"content-type": "text/html"},
-        "statusCode": 200,
-    }
-
-
+            
 def handler(event, context):
     evt = json.loads(event)
     print(evt)
+    print(context)
     path = evt["requestContext"]["http"]["path"]
     method = evt["requestContext"]["http"]["method"]
     query = evt.get("queryParameters", {})
+    
     if method != "GET":
-        return {
-            "body": "only support GET method\n",
-            "statusCode": 200,
-        }
+        return make_error_response("Only GET method is supported")
 
-    if path == "/gray":
-        return gray(query, context)
-
-    elif path == "/format":
-        return format(query, context)
-
-    elif path == "/watermark":
-        return watermark(query, context)
-
-    elif path == "/pinjie":
-        return pinjie(query, context)
-
-    else:
-        with open("index.html") as f:
-            return {
-                "body": f.read(),
-                "headers": {"content-type": "text/html"},
-                "statusCode": 200,
-            }
+    try:
+        if path == "/gray":
+            return gray(query, context)
+        elif path == "/format":
+            return format(query, context)
+        elif path == "/watermark":
+            return watermark(query, context)
+        elif path == "/pinjie":
+            return pinjie(query, context)
+        else:
+            return make_error_response("Invalid path")
+    except Exception as ex:
+        return make_error_response(ex)
